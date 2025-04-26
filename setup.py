31ddb1bf44abed67df724fe8c66eb007bd99ff0b
Dajesa <93830906+Dajesa@users.noreply.github.com>
2025-04-26 18:57:26 -0500

    Empaquetado de dependencias

---
+++ setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="edu.pad",
+    version="0.0.1",
+    author="Dawin Salazar Oviedo", 
+    author_email="dawin.salazar@iudigital.edu.co",
+    description = """Python Web Scraper & Data Extractor: a tool to automatically collect and parse web data
+                    Utilizes requests, BeautifulSoup, and lxml for efficient HTTP requests and HTML parsing.
+                    Supports configurable scraping rules, robust error handling, and dynamic content via Selenium.
+                    Outputs structured data in JSON and CSV formats for seamless integration with data pipelines.
+                    Modular architecture enables easy extension, scheduling, and integration into larger workflows.""",
+    py_modules=["Actividad1", "Actividad2", "Actividad3", "Actividad4"],
+    install_requires=[
+        "pandas",
+        "opnpyxl",
+        "requests",
+        "beautifulsoup4"
+    ]
+    
+  )
