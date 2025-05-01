172292ab5aefe11f854d38630a67d240e66b9c26
Dajesa <93830906+Dajesa@users.noreply.github.com>
2025-05-01 16:11:19 -0500

    cambios en el main

--- src/.edu_pad/main.py
+++ src/.edu_pad/main.py
@@ -5,7 +5,7 @@ import pandas as pd  # Import pandas for data manipulation
 
 def main():
     dataweb = Dataweb()  # Create an instance of the Dataweb class
-    df = pd.DataFrame()  # Initialize an empty DataFrame
+    
     df = dataweb.obtener_datos()  # Call the obtener_datos method to get data
     
     df.to_csv('data_web.csv', index=False)  # Save the DataFrame to a CSV file
