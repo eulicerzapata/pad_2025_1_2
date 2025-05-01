64b3114dfb9892f6d3fe3b3d0a62250cd9c10fe7
Dajesa <93830906+Dajesa@users.noreply.github.com>
2025-05-01 15:06:40 -0500

    clase main y clase datweb competas

--- src/.edu_pad/dataweb.py
+++ src/.edu_pad/dataweb.py
@@ -1,36 +1,83 @@
-import pandas as pd # Importing pandas for data manipulation
-import requests # Importing requests for making HTTP requests
-from bs4 import BeautifulSoup # Importing BeautifulSoup for web scraping
-import datetime # Importing datetime for date manipulation
+import pandas as pd  # Importing pandas for data manipulation
+import requests  # Importing requests for making HTTP requests
+from bs4 import BeautifulSoup  # Importing BeautifulSoup for web scraping
+import datetime  # Importing datetime for date manipulation
+
 
 class Dataweb:
     def __init__(self):
-        self.url = "https://es.finance.yahoo.com/quote/BTC-USD/history/" # URL of the dataweb
-        
+        self.url = "https://es.finance.yahoo.com/quote/BTC-USD/history/"  # URL of the dataweb
+
     def obtener_datos(self):
         # This method would contain the logic to scrape data from the URL
         try:
             # url, cabecera,
             headers = {
-              'User-Agent': 'Mozilla/5.0' 
-              }
-            
-            respuesta = requests.get(self.url, headers=headers) # Making a GET request to the URL  
+                'User-Agent': 'Mozilla/5.0'
+            }
+
+            # Making a GET request to the URL
+            respuesta = requests.get(self.url, headers=headers)
             if respuesta.status_code != 200:
-                  print("Error en la respuesta del servidor, url NO existe ") # Print error message if response is not 200
-              
+                # Print error message if response is not 200
+                print("Error en la respuesta del servidor, url NO existe ")
+
             # print(respuesta.text) # Print the status code of the response
-            soup = BeautifulSoup(respuesta.text, 'html.parser') # Parse the response text using BeautifulSoup
-            tabla = soup.select_one('div[data-testid="history-table"] table') # Select the table containing the data
-            print(tabla) # Print the table element  
-           
+            # Parse the response text using BeautifulSoup
+            soup = BeautifulSoup(respuesta.text, 'html.parser')
+            # Select the table containing the data
+            tabla = soup.select_one('div[data-testid="history-table"] table')
+            # print(tabla) # Print the table element
+
+            nombre_columnas = [th.get_text(strip=True)
+                               for th in tabla.thead.find_all('th')]
+            # List to store column names
+
+            filas = []  # List to store rows of data
+            for tr in tabla.tbody.find_all('tr'):
+                # List to store data of each row
+                columnas = [td.get_text(strip=True)
+                            for td in tr.find_all('td')]
+                # Check if the number of columns matches the number of column names
+                if len(columnas) == len(nombre_columnas):
+                    # Append the row data to the list of rows
+                    filas.append(columnas)
+
+            df = pd.DataFrame(filas, columns=nombre_columnas).rename(columns={
+                'Fecha': 'fecha',
+                'Abrir': 'abrir',
+                'Máx.': 'maximo',
+                'Mín.': 'minimo',
+                'CerrarPrecio de cierre ajustado para splits.': 'cerrar',
+                'Cierre ajustadoPrecio de cierre ajustado para splits y distribuciones de dividendos o plusvalías.': 'cierre ajustado',
+                'Volumen': 'volumen',
+            })  # Create a DataFrame from the rows and column names
+            # Save the DataFrame to an Excel file
+
+            df = self.convertir_numericos(df)  # Convert numeric columns to float
+
+            df.to_excel('dataweb_limpio.xlsx')
+
         except Exception as err:
-            print("Error en la funcion obtener datos") # Print the error message
-            
-        
-     
-        
-dw = Dataweb() # Dataweb object
-# print(dw.url) # Print the URL of the dataweb
-dw.obtener_datos() # Call the method to obtain data
-     
+            # Print the error message
+            print("Error en la funcion obtener datos")
+
+            print(err)
+            # Print the error message
+
+    def convertir_numericos(self,df=pd.DataFrame()):
+         df = df.copy()  # Create a copy of the DataFrame to avoid modifying the original one
+         if len(df) > 0:
+             for col in ('abrir', 'maximo', 'minimo',	'cerrar', 'cierre ajustado', 'volumen'):
+               df[col] = (
+                   df[col].str.replace(r'["\.", "" ]', '', regex=True)
+                   # Remove non-numeric characters
+                   .str.replace(r'[",", "." ]', '', regex=True)
+                   .astype(float)  # Convert to float
+                   )
+             return df  # Return the modified DataFrame
+           
+                           
+         
+         
+# if __name__ == "__main__":
---
+++ src/.edu_pad/main.py
@@ -0,0 +1,17 @@
+
+from dataweb import Dataweb  # Import the Dataweb class from dataweb module
+import pandas as pd  # Import pandas for data manipulation
+
+
+def main():
+    dataweb = Dataweb()  # Create an instance of the Dataweb class
+    df = pd.DataFrame()  # Initialize an empty DataFrame
+    df = dataweb.obtener_datos()  # Call the obtener_datos method to get data
+    
+    df.to_csv('data_web.csv', index=False)  # Save the DataFrame to a CSV file
+    
+if __name__ == "__main__":
+        main()  # Call the main function when the script is run
+   
+    
+        
